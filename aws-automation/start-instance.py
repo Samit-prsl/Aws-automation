d3895400c7c79616fe1bbcdc9e3d71b99bcf6dec
Samit <Samit.prsl@gmail.com>
2023-09-23 14:32:52 +0530

    Added start,stop and terminate instance automation

--- aws-automation/start-instance.py
+++ aws-automation/start-instance.py
@@ -1,7 +1,7 @@
 import boto3
 
 client = boto3.client('ec2')
-response = client.stop_instances(
+response = client.start_instances(
     InstanceIds=[
         'i-05c8b5248db0db3e9',
     ]
--- aws-automation/terminate-instance.py
+++ aws-automation/terminate-instance.py
@@ -1,7 +1,7 @@
 import boto3
 
 client = boto3.client('ec2')
-response = client.stop_instances(
+response = client.terminate_instances(
     InstanceIds=[
         'i-05c8b5248db0db3e9',
     ]
